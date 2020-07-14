91dc293dd99d6aa336cb2479cbdb090b541927a7
marsjava <marikannan.cvp@gmail.com>
2020-07-14 23:46:28 +0530

    Add the User model with create 2 new endpoints.

--- todo-stack-api/src/main/java/com/api/todo/controller/UserController.java
+++ todo-stack-api/src/main/java/com/api/todo/controller/UserController.java
@@ -1,12 +1,23 @@
 package com.api.todo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.todo.model.User;
 
 @RestController
 public class UserController {
     @GetMapping("/")
     public String getUserStatus() {
         return "Todo Management Response.";
+    }
+    @GetMapping("/user")
+    public User getUserBean() {
+    	return new User("Marikannan", "Shanmugam");
+    }
+    @GetMapping("/user/{firstname}/{lastname}")
+    public User getUserBeanWithParam(@PathVariable String firstname, @PathVariable String lastname) {
+    	return new User(firstname, lastname);
     }
 }
