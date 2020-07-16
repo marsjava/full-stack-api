d2e8782208a734a4da69da7ba25bcce6fc51c91b
marsjava <marikannan.cvp@gmail.com>
2020-07-16 12:24:10 +0530

    add new endpoint on usercontroller.

--- todo-stack-api/src/main/java/com/api/todo/controller/UserController.java
+++ todo-stack-api/src/main/java/com/api/todo/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.api.todo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.todo.model.User;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class UserController {
     @GetMapping("/")
@@ -14,7 +16,8 @@ public class UserController {
     }
     @GetMapping("/user")
     public User getUserBean() {
-    	return new User("Marikannan", "Shanmugam");
+    	return new User("Shanmugasundari", "Marikannan");
+//    	throw new RuntimeException("Something went wrong! Please contact to API Team.");
     }
     @GetMapping("/user/{firstname}/{lastname}")
     public User getUserBeanWithParam(@PathVariable String firstname, @PathVariable String lastname) {
